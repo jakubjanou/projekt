c602eab2dff6c582ac14bd5e40166dba5dfd055f
jakubjanou <132485797+jakubjanou@users.noreply.github.com>
2025-05-25 09:30:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = prompt("Kolik ti je let?");
+console.log(age - 18);
+alert("Ahoj milý čtenáři");
+confirm("Už si někdy pracoval s RDST?")
+
+
+console.log("Máš rád RDST komunikaci");
+console.log("Joooo");
+console.log("Tak to jseš pěknej zvrhlík");
+console.log("Úchyle");
